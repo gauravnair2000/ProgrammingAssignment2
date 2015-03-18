92349dd8700c0c3b9531bd482ba7d0551b8a025f
gauravnair2000 <gauravnair2000@gmail.com>
2015-03-18 21:22:56 +0530

    MatrixInverseAssignment2
    Programming assignment 2 for Course Era R language course

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do abcd
+## This program describes the usage of <<- operator which can be used to
+## assign a value to an object in an environment that is different from the
+## current environment. Following two functions create a special object that 
+## stores a square matrix and cache's its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing functions to
+## 1. Set the value of the matrix
+## 2. Get the value of the matric
+## 3. Set the value of the inverse
+## 4. Get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 	m<-NULL
@@ -20,7 +26,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve first checks if the inverse is already caluclated 
+## and returns the inverse from the cache. Also, displays message "getting cached data"
+## Otherwise it calculates the inverse via the setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
